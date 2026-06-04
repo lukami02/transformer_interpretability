62105081896d520426e0b2f7535bf9a30869c732
Doktorzivota <luka.milutinovic01@gmail.com>
2026-06-04 17:45:38 +0200

    refactor: clean up and simplify package imports in __init__.py

--- src/xai/__init__.py
+++ src/xai/__init__.py
@@ -1,6 +1,14 @@
-from .gradient import VanillaGradient, GradientXInput, IntegratedGradients, SmoothGrad, GradCAM
-from .attention import AttentionRollout, TransformerAttribution, GMAR
-from .lrp import LRP
+from .gradient.grad import VanillaGradient
+from .gradient.grad_input import GradientXInput
+from .gradient.integrated_grad import IntegratedGradients
+from .gradient.smooth_grad import SmoothGrad
+from .gradient.grad_cam import GradCAM
+
+from .attention.rollout import AttentionRollout
+from .attention.transformer_attribution import TransformerAttribution
+from .attention.gmar import GMAR
+
+from .lrp.lrp import LRP
  
 __all__ = [
     # Gradient-based
